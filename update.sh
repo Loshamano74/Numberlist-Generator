f2cca6776f938556caf298beb037b3526259e3a8
Loshamano74 <137676635+Loshamano74@users.noreply.github.com>
2023-12-13 10:23:14 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Stash any local changes in untracked files or directories
+git stash push --include-untracked
+
 # Fetch the latest changes from the remote repository
 git fetch origin
 
 # Reset the local branch to match the remote branch
 git reset --hard origin/main  # Replace 'main' with your branch name if different
 
-# Optionally, pull the changes (Use with caution as this might override local changes)
-# git pull origin main
+# Optionally, apply the stashed changes back, if any
+git stash apply
