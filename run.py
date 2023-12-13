949ffc616e4bb92fe619df5a5b1bb9d1c7ab8b14
Loshamano74 <137676635+Loshamano74@users.noreply.github.com>
2023-12-13 10:26:30 +0100

    Update run.py

--- run.py
+++ run.py
@@ -8,21 +8,35 @@ def generate_sequence(start, final, folder_name, file_name):
     file_path = os.path.join(folder_name, file_name + '.txt')
 
     with open(file_path, 'w') as arq:
-        for i in range(start, final+1):
+        for i in range(start, final + 1):
             temp = str(i)
             arq.write(temp + '\n')
 
     print(f"Number sequence saved in '{file_path}'")
 
-if __name__ == "__main__":
-    folder_name = input('Enter the folder name: ')
-    
+def get_input(prompt):
     while True:
-        start = int(input('Start number: '))
-        final = int(input('End number: '))
-        file_name = input('Insert a name for your number sequence file (or type "exit" to quit): ')
-        
-        if file_name.lower() == 'exit':
+        user_input = input(prompt + " (or type 'exit' to quit): ")
+        if user_input.lower() == 'exit':
+            return None
+        return user_input
+
+if __name__ == "__main__":
+    folder_name = get_input('Enter the folder name')
+
+    while folder_name is not None:
+        start = get_input('Start number')
+        if start is None:
             break
-        
+        start = int(start)
+
+        final = get_input('End number')
+        if final is None:
+            break
+        final = int(final)
+
+        file_name = get_input('Insert a name for your number sequence file')
+        if file_name is None:
+            break
+
         generate_sequence(start, final, folder_name, file_name)
